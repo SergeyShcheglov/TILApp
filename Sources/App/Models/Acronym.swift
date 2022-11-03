222b4e1402aacee3e4ef5f5bf28cf3b5ab5faa29
Sergey Shcheglov <97shcheglov@gmail.com>
2022-11-03 21:27:48 +0100

    add acronym model

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,4 +5,26 @@
 //  Created by Sergey Shcheglov on 03.11.2022.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+final class Acronym: Model {
+    static let schema: String = "acronyms"
+    
+    @ID
+    var id: UUID?
+    
+    @Field(key: "short")
+    var short: String
+    
+    @Field(key: "long")
+    var long: String 
+    
+    init() { }
+    
+    init(id: UUID? = nil, short: String, long: String) {
+        self.id = id
+        self.short = short
+        self.long = long
+    }
+}
