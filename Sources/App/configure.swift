66fee0d7cce73182d2807e01b2f7b0d2d2425ea9
Sergey Shcheglov <97shcheglov@gmail.com>
2022-11-04 13:17:56 +0100

    change db back to postgres

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,15 +1,24 @@
 import Fluent
-import FluentMongoDriver
+import FluentPostgresDriver
 import Vapor
+
 // configures your application
 public func configure(_ app: Application) throws {
-  // 2
-  try app.databases.use(.mongo(
-    connectionString: "mongodb://localhost:27017/vapor"),
-    as: .mongo)
-  app.migrations.add(CreateAcronym())
-  app.logger.logLevel = .debug
-  try app.autoMigrate().wait()
-  // register routes
-  try routes(app)
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST")
+        ?? "localhost",
+        username: Environment.get("DATABASE_USERNAME")
+        ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD")
+        ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME")
+        ?? "vapor_database"
+    ), as: .psql)
+    
+    app.migrations.add(CreateAcronym())
+    app.logger.logLevel = .debug
+    
+    try app.autoMigrate().wait()
+    // register routes
+    try routes(app)
 }
