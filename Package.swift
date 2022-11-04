f7a7241726b491c1800e9e38b8e64dc35bc75182
Sergey Shcheglov <97shcheglov@gmail.com>
2022-11-04 13:04:28 +0100

    change db to mongo

--- Package.swift
+++ Package.swift
@@ -2,43 +2,42 @@
 import PackageDescription
 
 let package = Package(
-    name: "TILApp",
-    platforms: [
-        .macOS(.v12)
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/vapor/vapor.git",
-            from: "4.0.0"),
-        .package(
-            url: "https://github.com/vapor/fluent.git",
-            from: "4.0.0"),
-        // 1
-        .package(
-            
-            url: "https://github.com/vapor/fluent-mysql-driver.git",
-            from: "4.0.0")
-    ],
-    targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Fluent", package: "fluent"),
-                // 2
-                .product(
-                    name: "FluentMySQLDriver",
-                    package: "fluent-mysql-driver"),
-                .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                .unsafeFlags(
-                    ["-cross-module-optimization"],
-                    .when(configuration: .release))
-            ] ),
-        .target(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
-    ]
+  name: "TILApp",
+  platforms: [
+    .macOS(.v12)
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/vapor/vapor.git",
+      from: "4.0.0"),
+    .package(
+      url: "https://github.com/vapor/fluent.git",
+      from: "4.0.0"),
+    // 1
+    .package(
+      url: "https://github.com/vapor/fluent-mongo-driver.git",
+      from: "1.0.0")
+], targets: [
+    .target(
+      name: "App",
+      dependencies: [
+        .product(name: "Fluent", package: "fluent"),
+        // 2
+        .product(
+          name: "FluentMongoDriver",
+          package: "fluent-mongo-driver"),
+        .product(name: "Vapor", package: "vapor")
+             ],
+             swiftSettings: [
+               .unsafeFlags(
+                 ["-cross-module-optimization"],
+                 .when(configuration: .release))
+             ]
+           ),
+           .target(name: "Run", dependencies: [.target(name: "App")]),
+           .testTarget(name: "AppTests", dependencies: [
+             .target(name: "App"),
+             .product(name: "XCTVapor", package: "vapor"),
+           ])
+       ]
 )
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,21 +1,15 @@
 import Fluent
-import FluentMySQLDriver
+import FluentMongoDriver
 import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
-    app.databases.use(.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME")
-        ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD")
-        ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME")
-        ?? "vapor_database",
-        tlsConfiguration: .forClient(certificateVerification: .none)
-    ), as: .mysql)
-    app.migrations.add(CreateAcronym())
-    app.logger.logLevel = .debug
-    try app.autoMigrate().wait()
-    // register routes
-    try routes(app)
+  // 2
+  try app.databases.use(.mongo(
+    connectionString: "mongodb://localhost:27017/vapor"),
+    as: .mongo)
+  app.migrations.add(CreateAcronym())
+  app.logger.logLevel = .debug
+  try app.autoMigrate().wait()
+  // register routes
+  try routes(app)
 }
