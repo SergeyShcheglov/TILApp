a4231ea6d29b07af4e73bf2fba9d8592ff2ec915
Sergey Shcheglov <97shcheglov@gmail.com>
2023-01-01 18:23:16 +0100

    add get, put, delete routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,4 +16,34 @@ func routes(_ app: Application) throws {
             acronym
         }
     }
+    
+    app.get("api", "acronyms") { req -> EventLoopFuture<[Acronym]> in
+        Acronym.query(on: req.db).all()
+    }
+    
+    app.get("api", "acronym", ":acronymID") { req -> EventLoopFuture<Acronym> in
+        Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    app.put("api", "acronyms", ":acronymID") { req -> EventLoopFuture<Acronym> in
+        let updatedAcronym = try req.content.decode(Acronym.self)
+        return Acronym.find(
+            req.parameters.get("acronymID"),
+            on: req.db)
+        .unwrap(or: Abort(.notFound)).flatMap { acronym in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req.db).map { acronym }
+        }
+    }
+    
+    app.delete("api", "acronyms", ":acronymID") { req -> EventLoopFuture<HTTPStatus> in
+        Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.delete(on: req.db)
+                    .transform(to: .noContent)
+            }
+    }
 }
