101f3f1ed0616cfb2481d62d6b3343d5d2ce4698
Sergey Shcheglov <97shcheglov@gmail.com>
2023-01-01 18:33:17 +0100

    search route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -46,4 +46,14 @@ func routes(_ app: Application) throws {
                     .transform(to: .noContent)
             }
     }
+    
+    app.get("api", "acronyms", "search") { req -> EventLoopFuture<[Acronym]> in
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return Acronym.query(on: req.db).group(.or) { or in
+            or.filter(\.$short == searchTerm)
+            or.filter(\.$long == searchTerm)
+        }.all()
+    }
 }
